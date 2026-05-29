ace65f4343dacc48c0df3d4f0b3e8cd417f019ec
zse4t7i <zse4t7i@outlook.com>
2026-05-29 21:05:40 +0800

    ✨ Add a basic timer.

--- Engine/Source/Util/Timer.cpp
+++ Engine/Source/Util/Timer.cpp
@@ -6,7 +6,17 @@ namespace
 //==============================================================================
 // Internal Variable
 //==============================================================================
+LARGE_INTEGER CounterFrequency;
+LARGE_INTEGER TimeStampStart;
+LARGE_INTEGER TimeStampCurrent;
+LARGE_INTEGER TimeStampPrevious;
 
+// Measured in seconds.
+FP64 TimeTotal{0.0};
+// Measured in milliseconds.
+FP64 TimeDelta{0.0};
+
+U64 FrameCounter{0};
 //==============================================================================
 // Internal Function
 //==============================================================================
@@ -19,11 +29,42 @@ namespace Acrylic::Timer
 {
 void Init()
 {
+    QueryPerformanceFrequency(&CounterFrequency);
+    QueryPerformanceCounter(&TimeStampStart);
+    TimeStampCurrent = TimeStampStart;
+    TimeStampPrevious = TimeStampStart;
 }
+void Update()
+{
+    TimeStampPrevious = TimeStampCurrent;
+    QueryPerformanceCounter(&TimeStampCurrent);
 
+    FrameCounter++;
+    TimeDelta = static_cast<FP64>(TimeStampCurrent.QuadPart -
+                                  TimeStampPrevious.QuadPart) /
+                static_cast<FP64>(CounterFrequency.QuadPart) * 1000;
+    TimeTotal =
+        static_cast<FP64>(TimeStampCurrent.QuadPart - TimeStampStart.QuadPart) /
+        static_cast<FP64>(CounterFrequency.QuadPart);
+}
 //==============================================================================
 // Accessors
 //==============================================================================
-
+auto GetDeltaTime() -> FP64
+{
+    return TimeDelta;
+}
+auto GetTotalTime() -> FP64
+{
+    return TimeTotal;
+}
+auto GetFPS() -> FP32
+{
+    return TimeDelta > 0.0 ? static_cast<FP32>(1.0F / TimeDelta) : 0.0F;
+}
+auto GetFrameCount() -> U64
+{
+    return FrameCounter;
+}
 } // namespace Acrylic::Timer
 #pragma endregion
--- Engine/Source/Util/Timer.hpp
+++ Engine/Source/Util/Timer.hpp
@@ -3,4 +3,10 @@
 namespace Acrylic::Timer
 {
 void Init();
+void Update();
+
+auto GetDeltaTime() -> FP64;
+auto GetTotalTime() -> FP64;
+auto GetFPS() -> FP32;
+auto GetFrameCount() -> U64;
 }
