5e336936b8ee0b5d6c310278b80bec3e0d5af7bd
zse4t7i <zse4t7i@outlook.com>
2026-07-28 19:31:44 +0800

    🚧 WIP

--- Source/Editor/xmake.lua
+++ Source/Editor/xmake.lua
@@ -2,6 +2,8 @@ target("Editor", function ()
     set_kind("binary")
     set_default(false)
     add_deps("Engine")
+
+    add_packages("directxtex")
     add_packages("tinygltf")
 
     add_files("**.cpp")
